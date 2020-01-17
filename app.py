b01294b5e21dee87183f06070af043d592d4722f
Beili Liu <nothatmakessense@gmail.com>
2020-01-17 17:27:59 -0500

    Create app database schema and migration files
    Create the schema for Artist, Venue, Genre, and Shows.
    Run flask db migrate and upgrade for database migration.

--- app.py
+++ app.py
@@ -5,8 +5,9 @@
 import json
 import dateutil.parser
 import babel
-from flask import Flask, render_template, request, Response, flash, redirect, url_for
+from flask import Flask, render_template, request, Response, flash, redirect, url_for, jsonify
 from flask_moment import Moment
+from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 import logging
 from logging import Formatter, FileHandler
@@ -20,6 +21,7 @@ app = Flask(__name__)
 moment = Moment(app)
 app.config.from_object('config')
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 # TODO: connect to a local postgresql database
 
@@ -28,34 +30,68 @@ db = SQLAlchemy(app)
 #----------------------------------------------------------------------------#
 
 class Venue(db.Model):
-    __tablename__ = 'Venue'
+    __tablename__ = 'venue'
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
-    city = db.Column(db.String(120))
-    state = db.Column(db.String(120))
-    address = db.Column(db.String(120))
-    phone = db.Column(db.String(120))
-    image_link = db.Column(db.String(500))
+    name = db.Column(db.String, nullable=False)
+    city = db.Column(db.String(120), nullable=False)
+    state = db.Column(db.String(120), nullable=False)
+    address = db.Column(db.String(120), nullable=False)
+    phone = db.Column(db.String(120), nullable=False)
+    image_link = db.Column(db.String(500), nullable=False)
     facebook_link = db.Column(db.String(120))
+    venue_genres = db.relationship('Venue_Genre', backref='venue',
+                                    lazy=True, cascade='all, delete-orphan')
+    shows = db.relationship('Show', backref='venue',
+                            lazy=True, cascade='all, delete-orphan')
 
     # TODO: implement any missing fields, as a database migration using Flask-Migrate
 
 class Artist(db.Model):
-    __tablename__ = 'Artist'
+    __tablename__ = 'artist'
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
-    city = db.Column(db.String(120))
-    state = db.Column(db.String(120))
-    phone = db.Column(db.String(120))
-    genres = db.Column(db.String(120))
-    image_link = db.Column(db.String(500))
+    name = db.Column(db.String, nullable=False)
+    city = db.Column(db.String(120), nullable=False)
+    state = db.Column(db.String(120), nullable=False)
+    phone = db.Column(db.String(120), nullable=False)
+    image_link = db.Column(db.String(500), nullable=False)
     facebook_link = db.Column(db.String(120))
+    artist_genres = db.relationship('Artist_Genre', backref='artist',
+                                    lazy=True, cascade='all, delete-orphan')
+    shows = db.relationship('Show', backref='artist',
+                            lazy=True, cascade='all, delete-orphan')
 
     # TODO: implement any missing fields, as a database migration using Flask-Migrate
 
 # TODO Implement Show and Artist models, and complete all model relationships and properties, as a database migration.
+
+# These Genre tables allow an artist or venue 
+# to be associated with multiple genres
+class Artist_Genre(db.Model):
+    __tablename__ = 'artist_genre'
+
+    artist_id = db.Column(db.Integer, db.ForeignKey('artist.id'),
+                          nullable=False, primary_key=True)
+    genre = db.Column(db.String(), nullable=False, primary_key=True)
+
+class Venue_Genre(db.Model):
+    __tablename__ = 'venue_genre'
+
+    venue_id = db.Column(db.Integer, db.ForeignKey('venue.id'),
+                          nullable=False, primary_key=True)
+    genre = db.Column(db.String(), nullable=False, primary_key=True)
+
+# The Show table, that is a child of Artist and Venue
+class Show(db.Model):
+    __tablename__ = 'show'
+
+    id = db.Column(db.Integer, primary_key=True)
+    artist_id = db.Column(db.Integer, db.ForeignKey('artist.id'), 
+                          nullable=False)
+    venue_id = db.Column(db.Integer, db.ForeignKey('venue.id'),
+                         nullable=False)
+    show_time = db.Column(db.DateTime, nullable=False)
 
 #----------------------------------------------------------------------------#
 # Filters.
@@ -80,6 +116,7 @@ def index():
   return render_template('pages/home.html')
 
 
+#  ----------------------------------------------------------------
 #  Venues
 #  ----------------------------------------------------------------
 
@@ -505,6 +542,7 @@ if not app.debug:
     file_handler.setLevel(logging.INFO)
     app.logger.addHandler(file_handler)
     app.logger.info('errors')
+
 
 #----------------------------------------------------------------------------#
 # Launch.
